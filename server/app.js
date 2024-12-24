a33151a9a7fa8023073283c9eb55ed7470eb6b33
Abhijeet Das <mns102720@gmail.com>
2024-12-24 23:14:12 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 
 const express = require("express")
 const app = express()
+
+
+
+
+
 const cors = require("cors")
 const colors = require("colors");
 // configs , dbconnect , cookieparser , environment variables
@@ -20,21 +25,36 @@ app.use(cors({
 const router = require("./routers/router")
 app.use("/api/v1", router)
 
+
+
 // Creating server using socket.io 
 const {createServer}=require("http")
 const {Server}=require("socket.io")
 const sequelize = require("./config/dbConnect")
 
-
+// socket io server
 const server = createServer(app)
-// const io = new Server(server, {
-//     cors:{
-//         origin:"*",
-//         methods:["*"]
+const io = new Server(server, {
+    cors:{
+        origin:"*",
+        methods:["*"],
+        credentials:true
         
-//     }
-// })
+    }
+})
 
+io.on('connection', (socket)=>{
+    console.log("New User connected to server ",socket.id);
+    socket.on("message",(message)=>{
+        console.log("message received",message);
+        io.emit("Recived-message",message)
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("User disconnected from server",socket.id);
+    })
+    
+})
 
 
 
