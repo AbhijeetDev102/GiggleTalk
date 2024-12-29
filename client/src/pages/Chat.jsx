aae4d0b26eef17b50ddc99f05c6fe88f70ef3fef
Abhijeet Das <130581860+AbhijeetDev102@users.noreply.github.com>
2024-12-29 23:21:55 +0530

    socket join group and logout button functionality

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -6,16 +6,28 @@ import UserChat from "../components/UserChat";
 import Messages from "../components/Messages";
 import { io } from "socket.io-client";
 import { useDispatch, useSelector } from "react-redux";
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import { setGroupId } from "../reduxStore/slices/socketInfo";
+ 
 
 
 const Chat = () => {
 
-  const dispatch = useDispatch();
-
+  
+const dispatch = useDispatch()
   const data = useSelector((messageState) => messageState.message.messageValue);
+  const groupId = useSelector((state)=>state.socket.groupId)
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const socket = useRef(null); // Use useRef to store the socket instance
+
+  
+  const [upcomingM , setUM] = useState({
+    content:"",
+    senderUserId:null
+  })
+
+
 
   useEffect(() => {
     socket.current = io(import.meta.env.VITE_BASE_URL); // Initialize socket connection
@@ -25,11 +37,21 @@ const Chat = () => {
       setLoading(false)
     });
 
+    socket.current.on("user-joined", (data)=>{
+      console.log(data);
+      
+    })
+
+
     socket.current.on("Recived-message", (message) => {
       console.log("Message Recived", message);
+      setUM(message)
     });
 
     return () => {
+      socket.current.off('connect')
+      socket.current.off('Recived-message')
+      
       socket.current.disconnect(() => {
         console.log("Disconnected from server");
       });
@@ -42,38 +64,23 @@ const Chat = () => {
     }
   }, [data]);
 
-  // const auth = async () => {
-  //   try {
-  //     // const response = await axios.post(
-  //     //   `${import.meta.env.VITE_BASE_URL}/api/v1/chat`,
-  //     //   {},
-  //     //   {
-  //     //     headers: {
-  //     //       Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //     //     },
-  //     //   }
-  //     // );
-    
-  //     //  dispatch(setUserinfo(response.data.data))
-      
-      
 
-      
+  useEffect(()=>{
+    if(groupId!=null){
+      socket.current.emit("join-group", groupId)
+    }
 
-  //     if (!response.data.status) {
-  //       navigate("/auth");
-  //     } else {
-  //       setLoading(false);
-  //     }
-  //   } catch (error) {
-  //     console.log(error.message);
-  //     navigate("/auth");
-  //   }
-  // };
+  }, [groupId])
+ 
+  
+  const logout = ()=>{
+    localStorage.clear()
+    navigate("/auth")
+    socket.current.emit('logout')
+    dispatch(setGroupId(null))
+  }
 
-  // useEffect(() => {
-  //   auth();
-  // }, []);
+  
 
   if (loading) {
     return (
@@ -94,13 +101,17 @@ const Chat = () => {
   } else {
     return (
       <div>
-        <div className="h-screen w-full bg-slate-950 flex justify-center items-center">
-          <div className="h-[98vh] w-[98vw] rounded-xl bg-slate-900 flex justify-around items-center">
-            <div className="h-[98%] w-[23%] bg-slate-800 rounded-xl">
-              <UserChat />
+        <div className="h-screen w-full theme-xl flex justify-center items-center">
+          <div className="h-[98vh] w-[99vw] rounded-xl flex justify-around items-center">
+            <div className="h-[98%] w-[5%] theme-md rounded-3xl flex flex-col justify-between p-3 py-7 items-center ">
+              <div></div>
+              <div className="h-9 w-full flex justify-center items-center rounded-lg text-white theme-lg hover:bg-red-500 cursor-pointer transition-all ease-in-out " onClick={logout}><LogoutRoundedIcon/></div>
             </div>
-            <div className="h-[98%] w-[75%] bg-slate-800 rounded-xl">
-              <Messages />
+            <div className="h-[98%] w-[28%] theme-md rounded-xl">
+              <UserChat setLoading={setLoading}/>
+            </div>
+            <div className="h-[98%] w-[64%] py-2 theme-md rounded-xl  md:block">
+              <Messages upcomingM={upcomingM} setUM={setUM}/>
             </div>
           </div>
         </div>
