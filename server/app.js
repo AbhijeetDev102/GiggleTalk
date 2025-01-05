a574e3ea0ea1a0d7ef5f539ae251343703fd8a8b
Abhijeet Das <130581860+AbhijeetDev102@users.noreply.github.com>
2025-01-05 12:03:52 +0530

    cors fix

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 //cors setup for comuniction with frontend
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"*",
     methods:["*"],
     credentials: true
 }))
