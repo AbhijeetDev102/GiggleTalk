3a832ee131adabd9eae88f016ae47247179d66e1
Abhijeet Das <130581860+AbhijeetDev102@users.noreply.github.com>
2025-01-06 20:54:41 +0530

    seperates the groupid to user message id for calling system

--- server/SocketIO/socket.js
+++ server/SocketIO/socket.js
@@ -14,35 +14,45 @@ const io = new Server(server, {
 io.on('connection',  (socket)=>{
     console.log("New User connected to server ",socket.id);
     
-    socket.on("join-group", (groupId) => {
-        if (socket.currentGroup) {
-            socket.leave(socket.currentGroup);
-            console.log(`User ${socket.id} left group ${socket.currentGroup}`);
-          }
-        socket.join(groupId);
-       
-        socket.currentGroup = groupId;
-        console.log(`User ${socket.id} joined group ${groupId}`);
+    socket.on("join-group", (groupIds) => {
+        groupIds.forEach(groupId => {
+            socket.join(groupId);
+            console.log(`User ${socket.id} joined group ${groupId}`);
+        });
     })
 
-    socket.on("message",async (message)=>{
-        console.log("message received",message);
-        if (socket.currentGroup) {
-            socket.broadcast.to(socket.currentGroup).emit("Recived-message", message);
-            // console.log(result);
+    socket.on("message",async ({data, groupId})=>{
+        console.log("message received",data);
+        if (groupId) {
+            socket.broadcast.to(groupId).emit("Recived-message", data);
+            
             
         }
     })
 
-    socket.on("deletedFromEveryOne", ()=>{
-        socket.broadcast.to(socket.currentGroup).emit("deleteMessage")
+    socket.on("deletedFromEveryOne", (groupId)=>{
+        socket.broadcast.to(groupId).emit("deleteMessage")
     })
 
-    socket.on('logout', ()=>{
-        socket.leave(socket.currentGroup)
-        console.log(`User ${socket.id} leaves the group ${socket.currentGroup}`);
+    socket.on('logout', (groupIds)=>{
+        groupIds.forEach(groupId => {
+            socket.leave(groupId)
+            console.log(`User ${socket.id} leaves the group ${groupId}`);
+        });
         
     })
+
+
+    socket.on("incomming-call", (data)=>{
+        socket.broadcast.to(socket.currentGroup).emit('receive-call', data)
+        console.log(data);
+        
+    })
+    socket.on("accept-call", (data)=>{
+        socket.broadcast.to(socket.currentGroup).emit('callAccepted', data)
+        console.log(data);
+
+    })  
 
     socket.on("disconnect",()=>{
         console.log("User disconnected from server",socket.id);
